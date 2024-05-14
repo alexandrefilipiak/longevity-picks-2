e545972f96399f6d010be1119aba6b0d8e9ea374
Alexandre <alexandrephili@gmail.com>
2024-05-14 23:58:31 +0200

    modify base schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@ import { db } from "./../server/db/index";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/83dcdb81-8b40-4c5f-b35f-b5eda313f14b-vegkb0.png",
-  "https://utfs.io/f/1ee55b32-1ea9-4476-b465-9c5d06e427a5-3zq5kh.webp",
-  "https://utfs.io/f/02b1e970-cf84-4475-a481-7a4af3b32e94-n555hx.webp"
-];
 
 const mockImages = mockUrls.map((url, index)=> ({
   id: index+1,
@@ -14,21 +9,20 @@ const mockImages = mockUrls.map((url, index)=> ({
 }));
 
 export default async function HomePage() {
-  const products = await db.query.products.findMany();
+  const products = await db.query.products.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
 
   console.log(products);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        <div key="1232">test</div>
-        { 
-          products.map((product)=> (<div key={product.id}>{product.name}</div>))
-        }
         {
-          mockImages.map((image)=> (
-            <div key={image.id} className="w-48">
-              <img src={image.url}/>
+          products.map((product)=> (
+            <div key={product.id} className="flex w-48 flex-col">
+              <img src={product.image_url}/>
+              <div>{product.name}</div>
             </div>
           ))
         }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -37,7 +37,8 @@ export const products = createTable(
   "product", 
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    image_url: varchar("image_url", { length: 256 }),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
