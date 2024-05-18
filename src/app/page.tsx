73b41de41f38ecdcd94a72294200a68bacf369c1
Alexandre <alexandrephili@gmail.com>
2024-05-18 14:55:50 +0200

    use Next.js Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { UploadButton } from "./utils/uploadthing";
 import { get } from "http";
 import { getMyProducts } from "~/server/queries";
 import { getProducts } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -14,11 +15,20 @@ async function Products() {
   console.log(products);
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4 justify-center">
           {
             products.map((product)=> (
-              <div key={product.id} className="flex w-48 flex-col">
-                <img src={product.image_url ?? undefined}/>
+              <div key={product.id} className="flex h-48 w-48 flex-col">
+                {product.image_url ? (
+                <Image src={product.image_url} 
+                style={{objectFit: "contain"}}
+                width={192}
+                height={192}
+                fill
+                alt={product.name}/>
+                ) : (
+                  <div>No image available</div>
+                )}
                 <div>{product.name}</div>
               </div>
             ))
