0f96405ff8bc276111f8a01713b7d3641c49f187
Alexandre <alexandrephili@gmail.com>
2024-05-19 16:34:04 +0200

    start enhancing upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -4,6 +4,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Sign } from "crypto";
 import { UploadButton } from "../utils/uploadthing";
 import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export function TopNav() {
         const router = useRouter();
@@ -12,14 +13,12 @@ export function TopNav() {
       <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
         <div className="font-bold">Longevity Picks</div>
         
-        <div className="flex gap-4">
+        <div className="flex flex-row items-center gap-4">
           <SignedOut>
             <SignInButton />
           </SignedOut>
             <SignedIn>
-                <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
-                    router.refresh();
-                }}/>
+                <SimpleUploadButton/>
                 <UserButton/>
             </SignedIn>
         </div>
