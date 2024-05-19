8da43672402bc435aca7e450e4a2959307e19e53
Alexandre <alexandrephili@gmail.com>
2024-05-19 16:42:56 +0200

    fix error with transpileClientSDK option in next.config.js

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const config = withSentryConfig(coreConfig,
         widenClientFileUpload: true,
     
         // Transpiles SDK to be compatible with IE11 (increases bundle size)
-        transpileClientSDK: true,
+        //transpileClientSDK: true,
     
         // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
         // This can increase your server load as well as your hosting bill.
