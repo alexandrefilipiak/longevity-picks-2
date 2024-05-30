1aecef12290c1ff9201bc6f1c9acd74fa07813d0
Alexandre <alexandrephili@gmail.com>
2024-05-30 16:11:09 +0200

    allow PostHog access to Clerk name and email of the user

--- src/app/_analytics/provider.tsx
+++ src/app/_analytics/provider.tsx
@@ -1,7 +1,9 @@
 // app/providers.js
 'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { useAuth, useUser } from '@clerk/nextjs';
+import { useEffect } from 'react';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
@@ -9,5 +11,29 @@ if (typeof window !== 'undefined') {
   })
 }
 export function CSPostHogProvider({ children } : { children: React.ReactNode}) {
+    return <PostHogProvider client={posthog}>
+        <CSPostHogWrapper>
+        {children}
+        </CSPostHogWrapper>
+        </PostHogProvider>
+}
+
+export function CSPostHogWrapper({ children } : { children: React.ReactNode}) {
+    const auth = useAuth();
+    const userInfo = useUser();
+
+    useEffect(() => {
+        if (userInfo.user) {
+            posthog.identify(userInfo.user.id, 
+                { email: userInfo.user.primaryEmailAddress,
+                username: userInfo.user.username,
+                fullName: userInfo.user.fullName
+                 }
+            );
+        } else if (!auth.isSignedIn) {
+            posthog.reset();
+        }
+    }, [auth, userInfo]);
+    
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
